364520c4b1720abe84ebcc6771dc5c10fe66ab1c
Narbok <narbok29@gmail.com>
2022-09-22 20:45:46 -0500

    clase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RequestsService } from './services/requests/requests.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'segunda-claseA';
+export class AppComponent implements OnInit {
+
+  name: string = "";
+  order: string = "";
+
+  constructor(public requestsService: RequestsService) {}
+
+  ngOnInit(): void {
+    this.button();
+  }
+
+  button() {
+    let btn = document.getElementById('btn');
+    let pokemon : HTMLInputElement = <HTMLInputElement> document.getElementById('pokemon');
+    btn?.addEventListener('click', () => {
+      this.requestsService.getPokemon(pokemon.value).subscribe({
+        next: (resp : any) => {
+          this.name = resp.name,
+          this.order = resp.order
+        }
+      })
+    });
+  }
 }
---
+++ src/app/services/requests/requests.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { map } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RequestsService {
+
+  constructor(public http: HttpClient) {
+
+  }
+
+  getPokemon(name: string){
+    return this.http.get('https://pokeapi.co/api/v2/pokemon/' + name).pipe( 
+      map(
+        (resp : any) => {
+          console.log(resp);
+          return {
+            name : resp.name,
+            order: resp.order
+          }
+        }
+      )
+    )
+  }
+}
